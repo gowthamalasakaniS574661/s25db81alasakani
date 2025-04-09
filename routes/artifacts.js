ef9055fa8659b26c50984d747be5c10921f282d0
gowthamalasakaniS574661 <S574661@nwmissouri.edu>
2025-04-09 14:24:16 -0500

    updated

--- routes/artifacts.js
+++ routes/artifacts.js
@@ -7,6 +7,6 @@ router.get("/api", artifact_controller.artifact_list);
 router.post("/", artifact_controller.artifact_create_post);
 router.put("/:id", artifact_controller.artifact_update_put);
 router.delete("/:id", artifact_controller.artifact_delete);
-router.get("/:id", artifact_controller.artifact_detail); // JSON detail
+router.get("/:id", artifact_controller.artifact_detail); // Must be last
 
 module.exports = router;
